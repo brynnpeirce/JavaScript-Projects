21a5fc1e63e7aec802ff2badad83c25ce52432f9
brynnpeirce <brynn98607@gmail.com>
2022-05-10 20:48:50 -0700

    += operator/call a function

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {  //first function called My_First_Function
+    var str='This is the button test!'; //variable is a string that will say "This is a button test"
+    document.getElementById('Button_Text').innerHTML=str; 
+}
+
+function myFunction() { //second function called myFuntion
+    var sentence = "I am learning"; 
+    sentence += " a lot from this book."; //This string will add on to the string above
+    document.getElementById("concatenate").innerHTML = sentence; //This will allow HTML to call on JS
+}
