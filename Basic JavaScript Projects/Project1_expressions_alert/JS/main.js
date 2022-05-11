bce8df97f9c5c894a972a93e3228d852cb9a1b94
brynnpeirce <brynn98607@gmail.com>
2022-05-10 19:42:06 -0700

    updated with all requirements

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,6 @@
-var tenants = UnitB="Cara", UnitA="Gregor";
-document.write(UnitB);
+window.alert('Apply now for 10% off!'); //this string will pop open when the page is first opened
+document.write('which tenant lives in unit B?'+' And what is their rent?'); //concatenate variables
+var tenants = UnitB="Cara", UnitA="Gregor"; //all tenants
+document.write(UnitB); //result is the tenant in unitb
+document.write(700 + 800); 
 
