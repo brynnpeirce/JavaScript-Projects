beb93b87653f9b7bf533e4b8f5f8b4e06d48a226
brynnpeirce <brynn98607@gmail.com>
2022-05-13 20:53:03 -0700

    in progress

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,18 @@
+document.write("11" + 7);
+document.write(typeof "animal")
+
+function my_Function() {
+    document.getElementById("A").innerHTML = 0/0
+}
+
+function number_One() {
+document.getElementById("B").innerHTML = isNaN('Hello');
+}
+
+function number_Two() {
+document.getElementById("C").innerHTML = isNaN(9);
+}
+
+document.write(-3E310)
+
+document.write(3E310)
